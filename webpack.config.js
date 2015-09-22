34e20dc817d485a94d7f24d2a7dc7b1d48006fad
zanjs <root@zanjs.com>
2015-09-22 22:12:43 +0800

    add jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 var path = require('path');
-
-
-module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
+var config = {
+  entry: path.resolve(__dirname, 'app/main.js')
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loader: 'jsx'
+    }, {
+      test: /\.css$/, // Only .css files
+      loader: 'style!css' // Run both loaders
+    }]
+  }
 };
+
+module.exports = config;
